7baa6420d3f081d0de2c648079a58a7455fc966e
Sandi_Navoiy <santes.au@gmail.com>
2023-08-24 18:14:30 +0300

    косметика

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.test import APITestCase
-from django.urls import reverse
 
 
 from users.models import User
